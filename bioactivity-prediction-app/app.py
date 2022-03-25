c255a30f02012d4628aa32026349d36a90aad838
aminbenmansour <amin.benmansour.10@gmail.com>
2022-03-26 00:59:05 +0100

    implement molecular descriptor calculator

--- bioactivity-prediction-app/app.py
+++ bioactivity-prediction-app/app.py
@@ -15,3 +15,11 @@ This app allows you to predict the bioactivity towards inhibting the `Acetylchol
 - Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
 ---
 """)
+
+# Molecular descriptor calculator
+def desc_calc():
+    # Performs the descriptor calculation
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    os.remove('molecule.smi')
