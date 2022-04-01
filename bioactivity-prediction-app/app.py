de93903a3ae1bdae6354e9002473b7af3e5c39a4
aminbenmansour <amin.benmansour.10@gmail.com>
2022-04-01 20:17:59 +0100

    more significant msg

--- bioactivity-prediction-app/app.py
+++ bioactivity-prediction-app/app.py
@@ -43,9 +43,6 @@ st.image(image, use_column_width=True)
 st.markdown("""
 # Bioactivity Prediction App (Acetylcholinesterase)
 This app allows you to predict the bioactivity towards inhibting the `Acetylcholinesterase` enzyme. `Acetylcholinesterase` is a drug target for Alzheimer's disease.
-**Credits**
-- App built in `Python` + `Streamlit`.
-- Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
 ---
 """)
 
